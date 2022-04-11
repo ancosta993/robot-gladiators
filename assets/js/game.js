7a04f6a54dcda5cfd485b462c66f6af5ff803000
Anthony Dcosta <adcosta2000@gmail.com>
2022-04-11 01:17:10 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,14 +31,15 @@ var fight = function(enemyName) {
        if (confirmSkip) {
          window.alert(playerName + ' has decided to skip this fight. Goodbye!');
          // subtract money from playerMoney for skipping
-         playerMoney = playerMoney - 10;
+         playerMoney = Math.max(playerMoney - 10);
          console.log("playerMoney", playerMoney)
          break;
        }
      }
  
      // remove enemy's health by subtracting the amount set in the playerAttack variable
-     enemyHealth = enemyHealth - playerAttack;
+     var damage = randomNumber(playerAttack-3, playerAttack);
+     enemyHealth = Math.max(0, enemyHealth - damage);
      console.log(
        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
      );
@@ -48,7 +49,7 @@ var fight = function(enemyName) {
        window.alert(enemyName + ' has died!');
  
        // award player money for winning
-       playerMoney = playerMoney + 20;
+       playerMoney = Math.max(0, playerMoney + 20);
        // leave while() loop since enemy is dead
        break;
      } else {
@@ -56,7 +57,8 @@ var fight = function(enemyName) {
      }
  
      // remove players's health by subtracting the amount set in the enemyAttack variable
-     playerHealth = playerHealth - enemyAttack;
+     var damage = randomNumber(enemyAttack - 3, enemyAttack);
+     playerHealth = Math.max(0, playerHealth - damage);
      console.log(
        enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
      );
@@ -86,7 +88,7 @@ var startGame = function(){
         var pickedEnemyName = enemyNames[i];
 
         // reset enemyHealth before starting new fight
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
 
         // use debugger to pause script from running and check what's going on at that moment in the code
         // debugger;
@@ -197,5 +199,11 @@ var shop = function(){
   }
 
 };
+
+var randomNumber = function(min, max){
+  var value = Math.floor(Math.random()* (max- min + 1))+min;
+
+  return value;
+}
 
 startGame();
